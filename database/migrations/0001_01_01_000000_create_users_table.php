2efbff40b5656db6629a763a1256f2082397005d
Auxilium <PaulVillacote.PGMR@gmail.com>
2024-12-14 21:01:29 +0800

    Initialize auth properties

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->unsignedBigInteger('facebook_id')->unique()->nullable();
+            $table->unsignedBigInteger('linkedin_id')->unique()->nullable();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
