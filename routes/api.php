e71fd92295bc767c62f107ea864447edbe843888
Auxilium <PaulVillacote.PGMR@gmail.com>
2024-12-15 11:48:48 +0800

    Removed git ignore

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/php-version', function () {
+    return phpversion();
+});
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Test
+Route::get('/php-version', function () {
+    return phpversion();
+});
